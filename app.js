55c7738fc27f6331dfd63ee79fc03ddfb0755052
jaedwein <jaedwein@gmail.com>
2022-03-30 23:07:20 -0300

    Replaced var by const

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var userRoutes = require("./routes/UserRoutes");
+const userRoutes = require("./routes/UserRoutes");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
